bbbd1a04d4540d8ade48dbf7fcd7f9283055cbc9
leejungmin4426 <you@example.com>
2025-01-10 21:02:58 +0900

    circle

--- circle.py
+++ circle.py
@@ -1,1 +1,26 @@
-#객체로 화면에 원 띄우고, 화살표로 움직이기 구현하기
+import pygame
+ball=pygame.Rect(screen_width // 2-16 // 2, screen_height // 2-16 // 2, 16, 16)
+ball_speed_x=5
+ball_speed_y=-5
+
+while True:
+    clock.tick(30)
+    screen.fill(BLACK)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygamea.K_LEFT:
+                paddle_dx=-5
+            elif event.key == pygame.K_RIGHT:
+                paddle_dx=5
+        
+        elif event.type == pygame.KEYUP:
+            if event.key == pygamea.K_LEFT:
+                paddle_dx=0
+            elif event.key == pygame.K_RIGHT:
+                paddle_dx=0
+
+    paddle.left += paddle_dx
